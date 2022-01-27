5e0505aa2c80277b7f1f04e0616d2f552fb32ab3
AbrorjonDev <axmadov.git@gmail.com>
2022-01-27 08:45:12 +0500

    Forms o'zgardi

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+
+
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
@@ -9,4 +11,5 @@ class RegistrationForm(UserCreationForm):
         model = User
         fields = ['email', 'username', 'password1', 'password2']
 from django.contrib.auth.views import LoginView
-# class LoginForm()
+
+
